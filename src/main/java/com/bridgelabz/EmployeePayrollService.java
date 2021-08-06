5fa495129f294b9a15bf1ca43618abea395d1d9a
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-08-06 18:58:19 +0530

    Added:To Store Employee Payroll into a File

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,42 +1,55 @@
 package com.bridgelabz;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    public enum IOService{
-        CONSOLE_IO,FILE_IO,DB_IO,REST_IO
-    }
-    private List<EmployeePayrollData> employeePayrolllist;
+    public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
+    private List<EmployeePayrollData> employeePayrollDataList;
 
-    public EmployeePayrollService(){ }
+    public EmployeePayrollService(){}
 
-    public EmployeePayrollService(List<EmployeePayrollData> employeePayrolllist) {
-        this.employeePayrolllist = employeePayrolllist;
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollDataList){
+        this.employeePayrollDataList = employeePayrollDataList;
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee PlayRoll Service");
         ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
-
+        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
     }
-
-    private void readEmployeePayrollData(Scanner consoleInputReader){
-        System.out.println("Enter Employee ID : ");
-        int id = consoleInputReader.nextInt();
-        System.out.println("enter Employee Name : ");
-        String name = consoleInputReader.next();
-        System.out.println("Enter Employee Salary : ");
-        double salary = consoleInputReader.nextDouble();
-        employeePayrolllist.add(new EmployeePayrollData(id,name,salary));
+    //method to read data
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee Id:");
+        int id=consoleInputReader.nextInt();
+        System.out.println("Enter Employee name:");
+        consoleInputReader.nextLine();
+        String name=consoleInputReader.nextLine();
+        System.out.println("Enter Employee salary:");
+        double salary=consoleInputReader.nextInt();
+        employeePayrollDataList.add(new EmployeePayrollData(id,name,salary));
     }
-
-    private void writeEmployeePayrollData(){
-        System.out.println("Writing Employee Payroll Roaster to Console\n" + employeePayrolllist);
+    //method to write data on console
+    public void writeEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("\nWriting Employee Payroll Roaster To console::\n"+employeePayrollDataList);
+        else if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollDataList);
+    }
+    //method to count entries in a file
+    public long countEntries() {
+        long entries = 0;
+        try {
+            entries = Files.lines(new File("payroll-file.txt").toPath()).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return entries;
     }
 }
